a6a49ca2a8bc091648d00b4ca6fe21217e956902
sparklyballs <sparklyballs@gmail.com>
2016-02-02 18:03:30 +0000

    fix curl error

--- init/20_fetch_source.sh
+++ init/20_fetch_source.sh
@@ -2,8 +2,7 @@
 
 [[ -d /tmp/guac_server ]] && rm -rf /tmp/guac_server
 
-curl -o /tmp/guac_source.tar.gz -L 
-http://sourceforge.net/projects/guacamole/files/current/source/guacamole-server-"${GUAC_VERSION}".tar.gz/download
+curl -o /tmp/guac_source.tar.gz -L http://sourceforge.net/projects/guacamole/files/current/source/guacamole-server-"${GUAC_VERSION}".tar.gz/download
 mkdir -p /tmp/guac_server
 tar xf /tmp/guac_source.tar.gz -C /tmp/guac_server --strip-components=1
 
